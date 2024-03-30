848aa1599f94ba72e48d4e86ad17e32c52e58d78
Cillian Hsueh <cillian-hsueh@hotmail.com>
2024-03-30 16:54:13 +0800

    env 參數透過 Yaml 取得

--- app/config.py
+++
@@ -1,6 +0,0 @@
-class MongoDBConfig:
-    # 連接本地端環境 mongodb
-    # URI = "mongodb://localhost:27017/"
-    # 連接 docker內 mongodb
-    URI = "mongodb://db:27017/"
-    DB_NAME = "test_api"
--- app/mongodb.py
+++ app/mongodb.py
@@ -1,10 +1,15 @@
 # mongodb.py
 from pymongo import MongoClient
-from app.config import MongoDBConfig
+from envyaml import EnvYAML
 
 class MongoDB:
     def __init__(self):
-        self.client = MongoClient(MongoDBConfig.URI)
-        self.db = self.client[MongoDBConfig.DB_NAME]
+        # 取得 YAML 檔環境變數參照自 .env 檔
+        env = EnvYAML("docker-compose.yml")
+        mongo_uri = env['services.mongodb.environment.MONGO_URI']
+        mongo_db = env['services.mongodb.environment.MONGO_DB_NAME']
+
+        self.client = MongoClient(mongo_uri)
+        self.db = self.client[mongo_db]
 
 mongo_db = MongoDB()
